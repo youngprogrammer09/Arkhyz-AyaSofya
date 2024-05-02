810116eac3298aa71d138fa29414dfc4da4ab1c0
youngprogrammer09 <154506178+youngprogrammer09@users.noreply.github.com>
2024-05-02 10:23:00 +0300

    Add files via upload

---
+++ js/slick.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.first_house').slick({autoplay: true,dots: true,});
+    $('.second_house').slick({autoplay: true,dots: true,});
+    $('.third_house').slick({autoplay: true,dots: true,});
+    $('.fourth_house').slick({autoplay: true,dots: true,});
+});
+      
